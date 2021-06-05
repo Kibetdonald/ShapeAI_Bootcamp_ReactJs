0ab602d303396c10478c9d465eb4fa220ae46d32
Don <50916200+Kibetdonald@users.noreply.github.com>
2021-06-05 13:17:55 +0300

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer(){
+    var thecurrentyear = new Date().getFullYear();
+  return(
+    <footer>
+        <p>Copyright @ {thecurrentyear}</p>
+    </footer>
+   )
+  }
+
+
+export default Footer
